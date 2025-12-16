85c7ce8f29d011968e20116494533c084c4652a0
Sid <sids@amd.com>
2025-12-16 15:02:19 +0800

    Minor variable name change

--- packages/vlm/gemma3/test_hf.py
+++ packages/vlm/gemma3/test_hf.py
@@ -14,7 +14,7 @@ model = Gemma3ForConditionalGeneration.from_pretrained(
 
 processor = AutoProcessor.from_pretrained(model_id)
 
-messages = [
+chat = [
     {
         "role": "system",
         "content": [{"type": "text", "text": "You are a helpful assistant."}]
@@ -29,14 +29,14 @@ messages = [
 ]
 
 inputs = processor.apply_chat_template(
-    messages, add_generation_prompt=True, tokenize=True,
+    chat, add_generation_prompt=True, tokenize=True,
     return_dict=True, return_tensors="pt"
 ).to(model.device, dtype=torch.bfloat16)
 
 input_len = inputs["input_ids"].shape[-1]
 
 with torch.inference_mode():
-    generation = model.generate(**inputs, max_new_tokens=100, do_sample=False)
+    generation = model.generate(**inputs, max_new_tokens=200, do_sample=False)
     generation = generation[0][input_len:]
 
 decoded = processor.decode(generation, skip_special_tokens=True)
